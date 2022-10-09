7eca3e62f6e97a7b063185869f739e09d32a606a
Jon Bell <github@lot23.com>
2022-10-09 18:47:03 +0000

    Adding

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="bg-black text-white rounded-sm border">
+      This is a header
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Header from "../components/Header.js";
+
+const list = [
+  "https://res.cloudinary.com/djp21wtxm/image/upload/v1656385240/i1600x842-SU6DLdzOL8cx_vieu5m.png",
+  "https://res.cloudinary.com/djp21wtxm/image/upload/v1656385400/i1600x534-gQXrSMkHUzzZ_hpzdoj.png",
+];
 
 export default function Home() {
-  return <div className={styles.container}>I did it</div>;
+  return (
+    <>
+      <Header />
+      <div className={styles.container}>I did it 2</div>
+      {list.map((item) => (
+        <div>{item}</div>
+      ))}
+    </>
+  );
 }
