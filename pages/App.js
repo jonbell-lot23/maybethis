a37036c5cf3292893a225fe2cbe44efd83919411
Jon Bell <github@lot23.com>
2022-10-24 00:06:24 +0000

    Trying stuff

---
+++ pages/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function App() {
+  const [selected, setSelected] = React.useState();
+
+  const bigarray = [];
+  for (let step = 1; step < 2000; step++) {
+	bigarray.push({ step });
+  }
+
+  return (
+	<div className="App">
+	  <header className="App-header p-8">
+		{bigarray.map((i) => (
+		  <div>{i.step}</div>
+		))}
+	  </header>
+	</div>
+  );
+}
+
+export default App;
