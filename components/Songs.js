743e4ff4d1e4a3b0d7ec40efa2f43136fff3cf93
Jon Bell <github@lot23.com>
2022-10-24 00:55:27 +0000

    Minor changes

---
+++ components/Songs.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Default(props) {
+  return;
+}
+
+export function Song(props) {
+  return (
+    <div
+      className={`p-2 h-screen w-full rounded-xl p-8 m-5 flex-inline ${props.color}`}
+    >
+      <h1 className="text-3xl">{props.text}</h1>
+    </div>
+  );
+}
+
+Song.defaultProps = {
+  color: "bg-gray-100",
+};
+
+export default Default;
